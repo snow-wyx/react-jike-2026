521b13b9716d88d276272d251c82841c033bf470
Yixue <625568979@qq.com>
2026-02-03 17:58:31 +0800

    二级路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,30 @@ import Layout from "@/pages/Layout";
 import Login from "@/pages/Login"
 import { createBrowserRouter } from 'react-router-dom'
 import { AuthRoute } from "@/components/AuthRoute";
+import Home from "@/pages/Home";
+import Article from "@/pages/Article";
+import Publish from "@/pages/Publish/publish";
+
 //路由配置实例
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <AuthRoute><Layout /></AuthRoute>
+    element: <AuthRoute><Layout /></AuthRoute>,
+    children: [
+      {
+        path: "home",
+        element: <Home />,
+      },
+      {
+        path: "article",
+        element: <Article />,
+      },
+      {
+        path: "Publish",
+        element: <Publish />,
+      },
+
+    ]
   },
   {
     path: "/login",
