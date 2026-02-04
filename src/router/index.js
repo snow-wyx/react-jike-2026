e1bfdd18a3317cb6bc2a02b4e3318ca6f580b988
Yixue <625568979@qq.com>
2026-02-04 17:05:52 +0800

    发布文章-基础解构搭建

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import { AuthRoute } from "@/components/AuthRoute";
 import Home from "@/pages/Home";
 import Article from "@/pages/Article";
-import Publish from "@/pages/Publish/publish";
+import Publish from "@/pages/Publish";
 
 //路由配置实例
 const router = createBrowserRouter([
